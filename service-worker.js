021fa53c626fc3c1a0449302bf493e39e565c856
Silvio Gajdosik <silvio.gajdosik@gmail.com>
2026-07-20 20:26:19 +0200

    Popuni tekst citanja za jos 8 nedjelja (23.8.-11.10.) + ocisti testfile.txt, bump v5

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
  * dobili novu verziju umjesto stare iz predmemorije.
  */
 
-const CACHE_NAME = "kalendar-misa-v4";
+const CACHE_NAME = "kalendar-misa-v5";
 
 const DATOTEKE_ZA_PREDMEMORIJU = [
   "./",
