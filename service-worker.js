d6b76817b267905e46b3cdbb993733383b5caa24
Silvio Gajdosik <silvio.gajdosik@gmail.com>
2026-07-21 06:53:32 +0200

    Backfill tekst citanja 07.12.2025-25.01.2026 (Saric PD), CACHE_NAME v7

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
  * dobili novu verziju umjesto stare iz predmemorije.
  */
 
-const CACHE_NAME = "kalendar-misa-v6";
+const CACHE_NAME = "kalendar-misa-v7";
 
 const DATOTEKE_ZA_PREDMEMORIJU = [
   "./",
